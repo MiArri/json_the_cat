5db46d6f83751de1c8d431eff519d26e218ac040
Elena Chernikova <lena@acvos.com>
2022-02-17 19:16:12 -0800

    refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,14 @@
 const request = require('request');
 
-const inputFrom =  process.argv.slice(2);
-let searchBreed = inputFrom[0];
-searchBreed = searchBreed.slice(0, 3);
-console.log(searchBreed);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error,response, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+    }
+    console.log(data);
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${searchBreed}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
+module.exports = {fetchBreedDescription};
 
-  const data = JSON.parse(body);
-  // console.log("data", data);
-  // console.log(typeof body);
-
-  console.log("first breed:", data[0].description);
-});
